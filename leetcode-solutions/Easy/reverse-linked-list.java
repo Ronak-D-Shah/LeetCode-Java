d1c9dd4c5200f5565e8e54e1c8b563aa40eca299
Ronak-D-Shah <shah.ronak2@northeastern.edu>
2025-07-16 03:33:21 -0400

    Solved - Reverse Linked List

--- leetcode-solutions/Easy/reverse-linked-list.java
+++ leetcode-solutions/Easy/reverse-linked-list.java
@@ -11,15 +11,16 @@
  * Space Complexity: O(1)
  */
 
-public class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-
 class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
