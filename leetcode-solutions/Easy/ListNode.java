19eb160eaef4b4de37ba601d667a6258d661f460
Ronak-D-Shah <shah.ronak2@northeastern.edu>
2025-07-16 03:26:57 -0400

    Solved - reverse linkedlist

---
+++ leetcode-solutions/Easy/ListNode.java
@@ -0,0 +1,34 @@
+/**
+ * LeetCode: 206. Reverse Linked List
+ * Difficulty: Easy
+ *
+ * Approach: Iterative
+ * - Use three pointers: prev, curr, next.
+ * - At each step, reverse the curr.next to point to prev.
+ * - Move the pointers one step forward.
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nxt = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        return prev;
+    }
+}
