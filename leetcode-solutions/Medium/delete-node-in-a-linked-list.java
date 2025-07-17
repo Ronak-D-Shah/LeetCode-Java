ee0d80a8f1ce46bb448b6b1b2ac66235ddd17a8e
Ronak-D-Shah <shah.ronak2@northeastern.edu>
2025-07-17 11:35:10 -0400

    Fix exception

--- leetcode-solutions/Medium/delete-node-in-a-linked-list.java
+++ leetcode-solutions/Medium/delete-node-in-a-linked-list.java
@@ -19,6 +19,14 @@
  */
 
 class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
     public void deleteNode(ListNode node) {
         // Copy the value from the next node into this node
         node.val = node.next.val;
